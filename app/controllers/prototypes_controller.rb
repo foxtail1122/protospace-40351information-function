61bd62c71a5a545d6ad831b443e358f6e524a5a9
foxtail1122 <160288948+foxtail1122@users.noreply.github.com>
2024-04-10 11:19:25 +0900

    initial commit

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,9 +1,9 @@
 class PrototypesController < ApplicationController
-  before_action :authenticate_user!, only: [:index, :show]
+  before_action :authenticate_user!, only: [:new,:edit,:destroy]
   before_action :move_to_index, except: [:index, :show]
-  protect_from_forgery with: :null_session
 
   def index
+    @prototypes = Prototype.includes(:user).order("created_at DESC")
   end
 
   def new
@@ -11,12 +11,11 @@ class PrototypesController < ApplicationController
   end
 
   def create
-    @prototype = Prototype.new(prototype_params)
+    @prototype = Prototype.create(prototype_params)
     if @prototype.save
       redirect_to root_path(@prototype)
     else
       render :new
-      @prototype = Prototype.includes(:user)
     end
   end
 
@@ -28,13 +27,15 @@ class PrototypesController < ApplicationController
 
   def edit
     @prototype = Prototype.find(params[:id])
+    unless @prototype.user_id == current_user.id
+      redirect_to action: :index
+    end
   end
 
   def update
     @prototype = Prototype.find(params[:id])
-    @prototype.update(prototype_params)
-    if @prototype.save
-      redirect_to root_path(@prototype)
+    if @prototype.update(prototype_params)
+      redirect_to prototype_path
     else
       render :edit
     end
@@ -46,14 +47,14 @@ class PrototypesController < ApplicationController
     redirect_to root_path
   end
 
-  private
-  def prototype_params
-    params.permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
-  end
-
   def move_to_index
     unless user_signed_in?
       redirect_to action: :index
     end
+  end
+
+  private
+  def prototype_params
+    params[:prototype].permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,7 @@ class User < ApplicationRecord
   validates :occupation, presence: true
   validates :position, presence: true
 
+  has_many :prototypes
+  has_many :comments, dependent: :destroy
+
 end
